fd3ca400bbf214cba7a94a2948f2c4e94a4dc20a
Franco Balich <franco.balich@gmail.com>
2023-07-29 20:45:38 -0300

    Feat: Create project

---
+++ src/AppGame.jsx
@@ -0,0 +1,11 @@
+
+function AppGame() {
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default AppGame
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppGame from './AppGame.jsx'
+import './styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppGame />
+  </React.StrictMode>,
+)
