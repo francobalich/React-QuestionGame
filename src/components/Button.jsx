0ea774597e226a7a263d08743c2c91e5398bf0e8
Franco Balich <franco.balich@gmail.com>
2023-07-29 23:58:09 -0300

    Feat: Add card reverse

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
-export const Button = ({ children }) => {
+export const Button = ({ children, onClick }) => {
   return (
-    <button className="w-[41px] h-[45px] pb-[7px] bg-lime-600 rounded-lg shadow border-white border-[3px] justify-center items-center gap-2.5 " >
+    <button onClick={onClick} className="w-[41px] h-[45px] pb-[7px] bg-lime-600 rounded-lg shadow border-white border-[3px] justify-center items-center gap-2.5 hover:brightness-110" >
       <div div className="w-[35px] h-[35px] bg-lime-500 rounded-[5px] shadow justify-center items-center flex" >
         {children}
       </div>
--- src/views/ReverseCard.jsx
+++ src/views/ReverseCard.jsx
@@ -1,6 +1,30 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { Button } from "../components/Button"
+import check from "../assets/check.svg"
+import { useNavigate } from "react-router-dom"
 
 export const ReverseCard = () => {
+  const [question, setQuestion] = useState("")
+  const [author, setAuthor] = useState("")
+
+  const navigate = useNavigate()
+
+  const handleClick = ()=>{
+    navigate(-1)
+  }
+  useEffect(() => {
+    setQuestion("¿Qué tal el stream de hoy?")
+    setAuthor("Franco Balich")
+  }, [])
+
   return (
-    <p>ReverseCard</p>
+    <div className="w-[80%] h-52 bg-yellow-200 rounded-[21px] shadow flex flex-col justify-evenly items-center">
+      <div className="flex flex-col justify-evenly items-center">
+        <p className="text-center text-[28px] font-medium tracking-wider" >{question}</p>
+        <p>Por: {author}</p>
+      </div>
+      <Button onClick={handleClick} ><img src={check} /></Button>
+    </div >
   )
 }
