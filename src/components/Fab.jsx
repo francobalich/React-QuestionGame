36333957a94cd096b7d4674e75de5ac85c380b17
Franco Balich <franco.balich@gmail.com>
2023-07-30 00:23:08 -0300

    Feat: Add variable Fab

--- src/components/Fab.jsx
+++ src/components/Fab.jsx
@@ -1,9 +1,16 @@
 
-export const Fab = ({ children, onClick, color="green" }) => {
+import { twMerge } from "tailwind-merge"
+import check from "../assets/check.svg"
+import plus from "../assets/plus.svg"
+
+export const Fab = ({ onClick, color="green" }) => {
+  const validateColor=()=>{
+    return color==="green"
+  }
   return (
-    <button onClick={onClick} className="w-[41px] h-[45px] pb-[7px] bg-lime-600 rounded-lg shadow border-white border-[3px] justify-center items-center gap-2.5 hover:brightness-110" >
+    <button onClick={onClick} className={twMerge("w-[41px] h-[45px] pb-[7px]  rounded-lg shadow border-white border-[3px] justify-center items-center gap-2.5 hover:brightness-110", color === "green" ? 'bg-lime-600' : 'bg-cyan-300')} >
       <div div className="w-[35px] h-[35px] bg-lime-500 rounded-[5px] shadow justify-center items-center flex" >
-        {children}
+        <img src={validateColor?check:plus} />
       </div>
     </button>
   )
