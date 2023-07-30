c81e67c79eedb3a3d6952bc2aa2c3b9c5ea1e0c8
Franco Balich <franco.balich@gmail.com>
2023-07-30 01:41:03 -0300

    Feat: Add createCards

--- src/views/GameCard.jsx
+++ src/views/GameCard.jsx
@@ -1,20 +1,39 @@
+import { useEffect } from "react"
 import { Card } from "../components/Card"
 import { Fab } from "../components/Fab"
+import questionData from '../data/questions.json'
+import { useState } from "react"
+
 //background: linear-gradient(90deg, #A659FE 0%, #6F53FD 100%);
 export const GameCard = () => {
+  const [questions, setQuestions] = useState([])
+
+  const createCards = (amount) => {
+    const cards = [];
+    let id = 1;
+    while (id <= amount) {
+      cards.push(<Card number={id} key={id} />)
+      id++
+    }
+    return cards
+  }
+
+  useEffect(() => {
+    const questionsUnread = questionData.filter(question => !question.status)
+      // .sort(() => 0.5 - Math.random())
+      .sort(() => ((Math.floor(Math.random() * 3) - 1)))
+      .slice(0, 8)
+    setQuestions(questionsUnread)
+    //TODO: Add useContext
+  }, [])
+
   return (
     <>
-    <section className="flex flex-wrap w-[504px] h-[220px] justify-center items-center gap-2.5 inline-flex">
-        <Card number={1} />
-        <Card number={2} />
-        <Card number={3} />
-        <Card number={4} />
-        <Card number={5} />
-        <Card number={6} />
-        <Card number={7} />
-        <Card number={8} />
-    </section>
-    <Fab className="fixed right-8 bottom-8" color="blue" />
+      <section className="flex flex-wrap w-[504px] h-[220px] justify-center items-center gap-2.5 inline-flex">
+
+        {createCards(8)}
+      </section>
+      <Fab className="fixed right-8 bottom-8" color="blue" />
     </>
   )
 }
