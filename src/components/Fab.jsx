a9b92297945cfbba1a605d1fa030fd9b4e700bb2
Franco Balich <franco.balich@gmail.com>
2023-07-30 00:46:51 -0300

    Feat: Add fab in correct position

--- src/components/Fab.jsx
+++ src/components/Fab.jsx
@@ -3,10 +3,10 @@ import { twMerge } from "tailwind-merge"
 import check from "../assets/check.svg"
 import plus from "../assets/plus.svg"
 
-export const Fab = ({ onClick, color = "green" }) => {
+export const Fab = ({ onClick, color = "green", className="" }) => {
 
   return (
-    <button onClick={onClick} className={twMerge("pb-1 rounded-lg shadow border-white border-4 justify-center items-center gap-2.5 hover:brightness-110", color === "green" ? 'bg-lime-600' : 'bg-sky-400')} >
+    <button onClick={onClick} className={twMerge("pb-1 rounded-lg shadow border-white border-4 justify-center items-center gap-2.5 hover:brightness-110", color === "green" ? 'bg-lime-600' : 'bg-sky-400',className)} >
       <div className={twMerge("w-9 h-9 rounded shadow justify-center items-center flex", color === "green" ? 'bg-lime-500' : 'bg-cyan-300')} >
         <img src={color === "green" ? check : plus} />
       </div>
--- src/views/GameCard.jsx
+++ src/views/GameCard.jsx
@@ -1,7 +1,9 @@
 import { Card } from "../components/Card"
+import { Fab } from "../components/Fab"
 //background: linear-gradient(90deg, #A659FE 0%, #6F53FD 100%);
 export const GameCard = () => {
   return (
+    <>
     <section className="flex flex-wrap w-[504px] h-[220px] justify-center items-center gap-2.5 inline-flex">
         <Card number={1} />
         <Card number={2} />
@@ -12,5 +14,7 @@ export const GameCard = () => {
         <Card number={7} />
         <Card number={8} />
     </section>
+    <Fab className="fixed right-8 bottom-8" color="blue" />
+    </>
   )
 }
