1219c00eea9471edface5b002d4e0e01f63aaacf
Franco Balich <franco.balich@gmail.com>
2023-07-29 23:29:23 -0300

    Feat: Add button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,10 @@
 
-export const Button = () => {
+export const Button = ({ children }) => {
   return (
-    <p>Button</p>
+    <button className="w-[41px] h-[45px] pb-[7px] bg-lime-600 rounded-lg shadow border-white border-[3px] justify-center items-center gap-2.5 " >
+      <div div className="w-[35px] h-[35px] bg-lime-500 rounded-[5px] shadow justify-center items-center flex" >
+        {children}
+      </div>
+    </button>
   )
 }
