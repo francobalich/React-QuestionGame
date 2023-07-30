396c85765ab2c40e7e6e062b46ccac7cc163cafc
Franco Balich <franco.balich@gmail.com>
2023-07-29 22:42:36 -0300

    Fix: Installation of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+         'hero-pattern': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8))",
+      },
+    },
   },
   plugins: [],
 }
